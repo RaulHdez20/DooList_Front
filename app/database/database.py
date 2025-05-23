231c98a2ae01a3474bb49932f8afe3ca716c4b87
RaulHdez20 <rulo19777@icloud.com>
2025-05-23 15:29:31 -0600

    Agregué endpoints para tareas y completadas

---
+++ app/database/database.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="RaulHdez",
+        password="Rulocraft7",
+        database="tareas_db"
+    )
